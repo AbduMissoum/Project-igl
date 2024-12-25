5315bb8e35867ea310d741ba30d4babea03a6772
AbduMissoum <ma_missoum@esi.dz>
2024-12-25 22:41:59 +0100

    added les soins

--- backend/api/les_soins/views.py
+++ backend/api/les_soins/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from .models import Soins
 from .serializers import SoinsSerializer,SoinsSerializerDetail,SoinsWithPatientIdSerializer,SoinsWithInfirmierSerializer
 from rest_framework.response import Response
 from dpi.models import Patient
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def soins_list(request):
     if request.method == 'GET':
         soins = Soins.objects.all()
@@ -26,7 +28,13 @@ def soins_list(request):
       except Exception as e:
         print(e)
         return Response( {"error": "An unexpected error occurred. Please try again later."}, status=status.HTTP_400_BAD_REQUEST)
-@api_view(['GET', 'PUT', 'DELETE'])
+
+
+
+
+
+@api_view(['GET', 'PATCH', 'DELETE'])
+@permission_classes([IsAuthenticated])
 def soins_detail(request, pk):
  if request.method == 'GET':
     try : 
@@ -41,9 +49,33 @@ def soins_detail(request, pk):
     except Exception as e:
         print(e)
         return Response( {"error": "An unexpected error occurred. Please try again later."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+ 
+ if request.method  =='PATCH':
+     try:
+           serializer = SoinsSerializer(Soins.objects.get(pk=pk), data=request.data, partial=True)
+           if serializer.is_valid():
+               serializer.save()
+               return Response(serializer.data)         
+     except Exception as e:
+        print(e)
+        return Response( {"error": "An unexpected error occurred. Please try again later."}, status=status.HTTP_400_BAD_REQUEST)
+    
+ if request.method == 'DELETE':
+    try:
+        soin = Soins.objects.get(pk=pk)
+        soin.delete()
+        return Response( {"message": "Soins deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+    except Soins.DoesNotExist as e:
+        print(e) 
+        
+        return Response( {"error": f"No soins with this id {pk}"}, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        print(e)
+        return Response( {"error": "An unexpected error occurred. Please try again later."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def soins_list_by_patient_id(request,patient_id):
  try:
     soins  = Soins.objects.filter(patient=patient_id)
@@ -54,6 +86,7 @@ def soins_list_by_patient_id(request,patient_id):
     return Response( {"error": "An unexpected error occurred. Please try again later."}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])  
+@permission_classes([IsAuthenticated])
 def soins_with_infirmier(request):
     try:
         soins = Soins.objects.filter(infirmier=request.user)
