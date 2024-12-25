d47ef6782b3cad05a59a818fdca1b07b94b0cb29
OussamaSouayeb <lo_souayeb@esi.dz>
2024-12-25 17:53:43 +0100

    ajout contact US

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 
-// import { PatientresumeComponent } from "./pages/patientresume/patientresume.component";
-import { ContactComponent } from './pages/contact/contact.component';
+
+import { LoginComponent } from "./pages/login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [  ContactComponent],
+  imports: [ LoginComponent],
   templateUrl: './app.component.html',  
 })
 export class AppComponent {
