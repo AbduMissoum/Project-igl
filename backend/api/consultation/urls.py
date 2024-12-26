299e8b7d71c14c6f24d816574396acf19a103227
imadkh14 <159920409+imadkh14@users.noreply.github.com>
2024-12-26 08:23:12 +0100

    Update urls.py

--- backend/api/consultation/urls.py
+++ backend/api/consultation/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('consultations/<int:pk>/', views.consultation_detail, name='consultation-detail'),
     path('consultations/by-date/', views.consultation_by_date, name='consultation-by-date'),
     path('consultations/by-dpi/<int:pk>/', views.consultation_by_dpi, name='consultation_by_dpi'),
+    path('consultations/medecin/<int:medecin_id>/', views.consultation_by_medecin, name='consultation_by_medecin'),
 ]
