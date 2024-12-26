0e7adbcfad0cdbd6756026e98ff353d5330c4a61
AbduMissoum <ma_missoum@esi.dz>
2024-12-26 22:58:34 +0100

    landed soinsDocs

--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -15,10 +15,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from rest_framework import permissions
+schema_view = get_schema_view(
+    openapi.Info(
+        title="My API",
+        default_version='v1',
+        description="API documentation for My Django project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@myapi.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
     path('patient/',include('dpi.urls')),
     path('auth/',include('authentication.urls')),
     path('',include('ordonnance.urls')),
