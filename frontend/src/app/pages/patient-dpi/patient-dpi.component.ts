baaedb738a97f7d16e6a5739c56be7ba653dc137
OussamaSouayeb <lo_souayeb@esi.dz>
2024-12-28 02:55:37 +0100

    generating QRcodes

--- frontend/src/app/pages/patient-dpi/patient-dpi.component.ts
+++ frontend/src/app/pages/patient-dpi/patient-dpi.component.ts
@@ -1,16 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { QRCodeComponent } from 'angularx-qrcode';
 
 @Component({
   selector: 'app-patient-dpi',
   standalone: true,  // Ce composant est Standalone
-  imports: [],
+  imports: [ CommonModule , QRCodeComponent  ],
   templateUrl: './patient-dpi.component.html',
   styleUrl: './patient-dpi.component.css'
 })
 export class PatientDPIComponent {
-  constructor() { }
+
+  nss: string = '123456789012345'; // Exemple de NSS
+  qrCodeData: string = ''; // Stockage du QR Code
 
   ngOnInit(): void {
-    console.log('Component initialized');}
+   
+  }
+
+  
+
+  constructor() { }
+
+  
 
 }
