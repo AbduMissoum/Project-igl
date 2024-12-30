70ec324fb3011563458375aaf710e431019366f9
imadkh14 <159920409+imadkh14@users.noreply.github.com>
2024-12-30 13:46:01 +0100

    Update serializers.py

--- backend/api/consultation/serializers.py
+++ backend/api/consultation/serializers.py
@@ -1,16 +1,19 @@
 from rest_framework import serializers
 from .models import Consultation
-from dpi.models import Dpi
-from authentication.models import CustomUser,Etablisement 
-
+from dpi.serializers import DpiSerializer,MedcinListSerializer
 
 class ConsultationSerializer(serializers.ModelSerializer):
-    dpi = serializers.PrimaryKeyRelatedField(queryset=Dpi.objects.all())
-    medecin = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all())
-    etablisement = serializers.PrimaryKeyRelatedField(queryset=Etablisement.objects.all())
+    class Meta:
+        model = Consultation
+        fields = ['id', 'medecin', 'dpi', 'etablisement', 'resume', 'la_date']
+
+class ConsultationDetailSerializer(serializers.ModelSerializer):
+    # Read-only fields for display purposes
+    medecin = MedcinListSerializer(read_only=True)
+    etablisement = serializers.CharField(source='etablisement.nom', read_only=True)
+    dpi = DpiSerializer(read_only=True)
 
     class Meta:
         model = Consultation
-
-        fields = ['id','dpi','medecin','etablisement','la_date','resume']
+        fields = ['id', 'medecin', 'dpi', 'etablisement', 'resume', 'la_date']
 
