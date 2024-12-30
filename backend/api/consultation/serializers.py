b0a211a39bb839c9be2a8d7ce7c80615acd9c80b
imadkh14 <159920409+imadkh14@users.noreply.github.com>
2024-12-30 17:37:17 +0100

    Update serializers.py

--- backend/api/consultation/serializers.py
+++ backend/api/consultation/serializers.py
@@ -1,14 +1,21 @@
 from rest_framework import serializers
 from .models import Consultation
-from dpi.models import Patient,Dpi
 from dpi.serializers import DpiSerializer,MedcinListSerializer
+from dpi.models import Dpi,Patient
 
 class ConsultationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Consultation
         fields = ['id', 'medecin', 'dpi', 'etablisement', 'resume', 'la_date']
 
+class ConsultationDpiSerializer(serializers.ModelSerializer):
+    # Read-only fields for display purposes
+    medecin = MedcinListSerializer(read_only=True)
+    etablisement = serializers.CharField(source='etablisement.nom', read_only=True)
 
+    class Meta:
+        model = Consultation
+        fields = ['id', 'medecin', 'etablisement', 'resume', 'la_date']
 
     
 class PatientSeriliazerConsultation(serializers.ModelSerializer):
@@ -16,11 +23,13 @@ class PatientSeriliazerConsultation(serializers.ModelSerializer):
     class Meta:
         model = Patient
         fields = ['id', 'NSS', 'nom', 'prenom']
+
 class DpiSerializerConsultation(serializers.ModelSerializer):
     id = PatientSeriliazerConsultation(read_only=True)
     class Meta:
         model = Dpi
         fields = ['id']
+
 class ConsultationDetailSerializer(serializers.ModelSerializer):
     # Read-only fields for display purposes
     medecin = MedcinListSerializer(read_only=True)
@@ -31,7 +40,13 @@ class ConsultationDetailSerializer(serializers.ModelSerializer):
         model = Consultation
         fields = ['id', 'medecin', 'dpi', 'etablisement', 'resume', 'la_date']
 
-
+class ConsultationwithDpiSerializer(serializers.ModelSerializer):
+    # Read-only fields for display purposes
+    medecin = MedcinListSerializer(read_only=True)
+    etablisement = serializers.CharField(source='etablisement.nom', read_only=True)
+    class Meta:
+        model = Consultation
+        fields = ['id', 'medecin','dpi','etablisement', 'resume', 'la_date']
 
 
 
