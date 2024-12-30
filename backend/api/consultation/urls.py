dac119ed806a53c10995eb8de65b91ec7f38facc
imadkh14 <159920409+imadkh14@users.noreply.github.com>
2024-12-30 13:45:29 +0100

    Update urls.py

--- backend/api/consultation/urls.py
+++ backend/api/consultation/urls.py
@@ -4,9 +4,27 @@ from . import views
 urlpatterns = [
     path('consultations/', views.consultation_list, name='consultation-list'),
     path('consultations/<int:pk>/', views.consultation_detail, name='consultation-detail'),
-    path('consultations/by-date/<str:date>/', views.consultation_by_date, name='consultation-by-date'),
     path('consultations/by-dpi/<int:pk>/', views.consultation_by_dpi, name='consultation_by_dpi'),
-    path('consultations/medecin/<int:medecin_id>/', views.consultation_by_medecin, name='consultation_by_medecin'),
-
+    path('consultations/<int:pk>/resume/', views.consultation_resume, name='consultation_resume'),
 ]
-
+"""{
+    "etablisement": "esi",
+    "dpi": 3,
+    "la_date": "2024-06-01"
+   }
+    {
+    "id": 8,
+    "medecin": {
+        "id": 13,
+        "username": "username",
+        "email": "imad@esi.dz"
+    },
+    "dpi": {
+        "id": 3,
+        "qr_code": null
+    },
+    "etablisement": "esi",
+    "resume": null,
+    "la_date": "2024-06-25"
+}
+}"""
