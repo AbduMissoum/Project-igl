a5936c8092a704de3d34e0b29de303d4ff7f4939
imadkh14 <159920409+imadkh14@users.noreply.github.com>
2025-01-03 15:32:18 +0100

    Update models.py

--- backend/api/ordonnance/models.py
+++ backend/api/ordonnance/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from consultation.models import Consultation
+
 # Create your models here.
 class Medicament(models.Model):
     id = models.AutoField(primary_key=True)
@@ -12,18 +13,17 @@ class Medicament(models.Model):
 class Ordonnance(models.Model):
     id = models.AutoField(primary_key=True)
     valide = models.BooleanField(default=False)
-    consultation  = models.ForeignKey(Consultation, on_delete=models.CASCADE,default=None)
+    consultation = models.ForeignKey(Consultation, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return f"Ordonnance {self.id} - {'Valid' if self.valide else 'Invalid'}"
-
 
 class Traitement(models.Model):
     id = models.AutoField(primary_key=True)
     la_dose = models.CharField(max_length=255)
     la_durre = models.CharField(max_length=255, null=True, blank=True)
     medicament = models.ForeignKey(Medicament, on_delete=models.CASCADE)
-    ordonnance = models.ForeignKey(Ordonnance, on_delete=models.CASCADE)
+    ordonnance = models.ForeignKey(Ordonnance, on_delete=models.CASCADE, related_name='traitements')  # ajout du related_name
 
     def __str__(self):
         return f"Traitement {self.id} - Dose: {self.la_dose}"
