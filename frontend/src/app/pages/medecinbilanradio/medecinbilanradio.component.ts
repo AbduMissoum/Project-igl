3cf1bd7bfbad758cc118adb329dee0e203cc808c
OussamaSouayeb <lo_souayeb@esi.dz>
2025-01-05 10:37:20 +0100

    alertes bilanradio

--- frontend/src/app/pages/medecinbilanradio/medecinbilanradio.component.ts
+++ frontend/src/app/pages/medecinbilanradio/medecinbilanradio.component.ts
@@ -20,6 +20,16 @@ export class MedecinbilanradioComponent implements OnInit {
   examenImage: string | null = null;
   compteRendu: string | null = null;
 
+  // Style mémorisé pour les alertes SweetAlert2
+  swalOptions = {
+    customClass: {
+      popup: 'rounded-[40px] shadow-lg bg-clair p-6 text-center', // Style du popup
+      title: 'text-2xl font-bold text-fonce', // Style du titre
+      confirmButton: 'bg-fonce text-white w-auto px-8 rounded-[40px]', // Style du bouton de confirmation
+      cancelButton: 'bg-gray-200 text-fonce px-8 rounded-[40px]', // Style du bouton d'annulation
+    },
+  };
+
   constructor(
     private http: HttpClient,
     private authService: AuthService,
@@ -27,7 +37,6 @@ export class MedecinbilanradioComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    
     // Récupérer les détails de la consultation via SharedService
     this.sharedService.consultationDetails$.subscribe((data) => {
       this.consultationDetails = data;
@@ -50,11 +59,23 @@ export class MedecinbilanradioComponent implements OnInit {
   submitRequest(): void {
     if (!this.consultationDetails || !this.consultationDetails.id) {
       console.error('ID de consultation manquante');
+      Swal.fire({
+        title: 'Erreur',
+        text: 'ID de consultation manquante',
+        icon: 'error',
+        ...this.swalOptions
+      });
       return;
     }
 
     if (!this.radioName) {
       console.error('Le type du Radio est manquant');
+      Swal.fire({
+        title: 'Erreur',
+        text: 'Le type du Radio est manquant',
+        icon: 'error',
+        ...this.swalOptions
+      });
       return;
     }
 
@@ -71,11 +92,21 @@ export class MedecinbilanradioComponent implements OnInit {
       next: (response: any) => {
         console.log('Réponse de l\'API:', response);
         this.isPopupVisible = false; // Fermer le pop-up après soumission
-        Swal.fire('Succès', 'Demande envoyée avec succès.', 'success');
+        Swal.fire({
+          title: 'Succès',
+          text: 'Demande envoyée avec succès.',
+          icon: 'success',
+          ...this.swalOptions
+        });
       },
       error: (error) => {
         console.error('Erreur lors de la demande:', error);
-        alert('Erreur lors de la demande. Veuillez vérifier les informations et réessayer.');
+        Swal.fire({
+          title: 'Erreur',
+          text: 'Erreur lors de la demande. Veuillez vérifier les informations et réessayer.',
+          icon: 'error',
+          ...this.swalOptions
+        });
       }
     });
   }
@@ -90,14 +121,11 @@ export class MedecinbilanradioComponent implements OnInit {
       .subscribe({
         next: (response: any) => {
           console.log('Données de l\'examen:', response);
-          console.log('response', response.message.bilan.compte_rendu)
-          console.log('response', response.message.examen_image)
           this.compteRendu = response?.message.bilan?.compte_rendu || 'Aucun compte rendu disponible.';
           this.examenImage = `http://localhost:8000${response?.message.examen_image }` || null;
         },
         error: (err) => {
           console.error('Erreur lors de la récupération des données:', err);
-         
         },
       });
   }
